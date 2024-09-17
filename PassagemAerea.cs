7fc5b81143a7d3d5b423203b36ee89b8b1e4d1c0
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-09-16 23:51:30 -0300

    criada a casse passagem aérea e passagem econômica

---
+++ PassagemAerea.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassagensAereas
+{
+    public class PassagemAerea
+    {
+
+        public string  Origem { get; set; }
+        public string Destino { get; set; }
+            public string Data { get; set; }
+        public int Quantidades { get; set; }
+        public doublie Preco { get; set; }
+
+        public PassagemAerea(string origem, string destino, string data, int quantidade, doublie preco)
+        {
+            Origem = origem;
+            Destino = destino;
+            Data = data;
+            Quantidades = quantidade;
+            Preco = preco;
+        }
+        public double CalcularPreco() => Preco * Quantidades;
+    }
+}
